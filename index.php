05045cd74060e26460d77ae10446f65068f0a89c
pelukissenja <99958892+pelukissenja@users.noreply.github.com>
2022-02-24 19:04:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#282d38">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sekolahku</title>
+    <link rel="stylesheet" href="./assets/css/app.css">
+</head>
+<body>
+    <nav>
+        <div class="brand">SD N Cibugel I</div>
+        <ul>
+            <li>
+                <a href="#utama">About</a>
+            </li>
+            <li>
+                <a href="#unggulan">Program</a>
+            </li>
+            <li>
+                <a href="#guru">Guru</a>
+            </li>
+            <li>
+                <a href="#siswa">Siswa</a>
+            </li>
+            <li>
+                <a href="#testi">Testimoni</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="menu" onclick="showHide();">&#x2630;</div>
+    <div class="mobile">
+        <ul>
+            <li class="brand">
+                SD N Cibugel I
+            </li>
+            <li>
+                <a href="#utama">About</a>
+            </li>
+            <li>
+                <a href="#unggulan">Program</a>
+            </li>
+            <li>
+                <a href="#guru">Guru</a>
+            </li>
+            <li>
+                <a href="#siswa">Siswa</a>
+            </li>
+            <li>
+                <a href="#testi">Testimoni</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="hero">
+            <h1>SDN CIBUGEL I</h1>
+            <p>
+                Jl. Raya Bojong Loa Kp. Cibugel RT 04/03 Ds. Cibugel Kec. Cisoka
+            </p>
+        </div>
+    </div>
+    <section id="utama">
+        <div class="wrapper">
+            <div class="pengantar">
+                <p class="greet">
+                   Sekolah Dasar Negeri Cibugel I merupakan sekolah yang menyelenggarakan GSM (Gerakan Sekolah Menyenangkan) dan berpartisipasi dalam sekolah penggerak. 
+                </p>
+                <button class="btn-big" onclick="daftar();">Daftar Sekarang!</button>
+            </div>
+            <div class="picture">
+                <img src="https://images.unsplash.com/photo-1628243426757-b092ba839ff9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
+            </div>
+        </div>
+    </section>
+    <section id="unggulan">
+        <div class="wrapper">
+            <div class="box">
+                <div class="box-1">
+                    <img src="https://images.unsplash.com/photo-1509191436522-d296cf87d244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80" alt="">
+                    <p class="deskripsi">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, necessitatibus. Libero natus accusamus, deleniti placeat accusantium officia in, laudantium voluptas, doloribus ab sequi ex veritatis eligendi pariatur quisquam nihil numquam.
+                    </p>
+                </div>
+                <div class="box-1">
+                    <img src="https://images.unsplash.com/photo-1509191436522-d296cf87d244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80" alt="">
+                    <p class="deskripsi">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, necessitatibus. Libero natus accusamus, deleniti placeat accusantium officia in, laudantium voluptas, doloribus ab sequi ex veritatis eligendi pariatur quisquam nihil numquam.
+                    </p>
+                </div>
+                <div class="box-1">
+                    <img src="https://images.unsplash.com/photo-1509191436522-d296cf87d244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80" alt="">
+                    <p class="deskripsi">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, necessitatibus. Libero natus accusamus, deleniti placeat accusantium officia in, laudantium voluptas, doloribus ab sequi ex veritatis eligendi pariatur quisquam nihil numquam.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="testi">
+        <div class="wrapper">
+            <div class="picture">
+                <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="">
+            </div>
+            <div class="pengantar">
+                <p class="greet">
+                    Dengan dukungan lingkungan yang bersih dan sehat serta bimbingan bapak / ibu guru yang tidak pernah lelah, anak saya mendapatkan tempat yang layak untuk menimba ilmu.
+                </p>
+                <p style="text-align:right;">
+                    Orang Tua Murid
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="guru">
+        <div class="content">
+            <div class="guru-pic">
+                <img src="https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=466&q=80" alt="">
+            </div>
+            <div class="comment">
+                <blockquote>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ut commodi inventore porro ratione ullam dolorum. Aliquid, officia, nisi magnam, ab dicta recusandae perferendis similique veritatis architecto corporis cumque ea.
+                </blockquote>
+            </div>
+        </div>
+    </section>
+    <section id="siswa">
+        <div class="wrapper">
+            <div class="galery">
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+                <div class="item">
+                    <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" title="Dane">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <div class="wrapper">
+            <form action="" method="post" id="contactForm">
+                <div class="group">
+                    <input type="text" class="with-control" placeholder="Nama Anda" name="nama">
+                </div>
+                <div class="group">
+                    <input type="email" class="with-control" placeholder="Email Anda" name="email">
+                </div>
+                <div class="group">
+                    <input type="text" class="with-control" placeholder="Nomor HP Anda" name="hp">
+                </div>
+                <div class="group">
+                    <textarea name="desk" id="desk" cols="30" rows="10" placeholder="Tuliskan yang ingin anda tanyakan dengan jelas." class="with-control"></textarea>
+                </div>
+                <div class="group">
+                    <button type="button">Simpan</button>
+                </div>
+            </form>
+            <div class="icon">
+                <div class="sosmed-fb">
+                    <h5>Facebook</h5>
+                </div>
+                <div class="sosmed-tw">
+                    <h5 >Twitter</h5>
+                </div>
+                <div class="sosmed-ig">
+                    <h5>Instagram</h5>
+                </div>
+                <div class="sosmed-link">
+                    <h5>LinkedIn</h5>
+                </div>
+                <div class="sosmed-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0949683185563!2d106.41621430909933!3d-6.251216262477938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e420517c7097d5f%3A0xf33d2d476ebc9892!2sSDN%20CIBUGEL%20I!5e0!3m2!1sen!2sid!4v1645097779127!5m2!1sen!2sid" width="100%" height="auto" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>
+            Allright Reserved &copy; <a href="">Virtual Digital Solution</a>
+        </p>
+    </footer>
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
